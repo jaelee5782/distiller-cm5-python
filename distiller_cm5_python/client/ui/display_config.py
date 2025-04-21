c60464b75e9915c5319bd729b4a7d991e805bd6c
utsavbalar1231 <utsavbalar1231@gmail.com>
2025-04-21 21:53:28 +0530

    perf(config): Reduce frame buffer size
    Change-Id: I237880dcca4939107c3a3755bd41509f37974b9a
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- distiller_cm5_python/client/ui/display_config.py
+++ distiller_cm5_python/client/ui/display_config.py
@@ -4,7 +4,7 @@ config = {
         "eink_full_refresh_interval": 30,
         "eink_refresh_interval": 1500,
         "eink_enabled": True,
-        "eink_buffer_size": 2,
+        "eink_buffer_size": 1,  # Reduced buffer size to save memory
         "dark_mode": False,
         "width": 240,
         "height": 416
